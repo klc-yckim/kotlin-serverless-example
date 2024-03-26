10cbfb9db042fce073edcbf5eee330c571715975
klc-yckim <yckim@korealaundry.com>
2024-03-26 14:21:07 +0900

    :bug: 테스트 실패 수정

--- src/test/kotlin/com/example/ApplicationTest.kt
+++ src/test/kotlin/com/example/ApplicationTest.kt
@@ -1,8 +1,12 @@
 package com.example
 
+import com.example.persistences.DatabaseSingleton
 import com.example.plugins.configureRouting
 import io.ktor.client.request.*
 import io.ktor.http.*
+import io.ktor.serialization.jackson.*
+import io.ktor.server.application.*
+import io.ktor.server.plugins.contentnegotiation.*
 import io.ktor.server.testing.*
 import kotlin.test.Test
 import kotlin.test.assertEquals
@@ -12,9 +16,13 @@ class ApplicationTest {
     fun testRoot() = testApplication {
         application {
 //            configureSecurity()
+            DatabaseSingleton.init()
+            install(ContentNegotiation) {
+                jackson()
+            }
             configureRouting()
         }
-        client.get("/").apply {
+        client.get("/v1/articles").apply {
             assertEquals(HttpStatusCode.OK, status)
         }
     }
