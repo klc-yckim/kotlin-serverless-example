dbdad04ea297e4dcac874fda7037820e2a426f58
klc-yckim <yckim@korealaundry.com>
2024-03-26 09:39:41 +0900

    :recycle: JWT 시크릿 값 환경 변수 처리하도록 변경

--- src/main/kotlin/com/example/plugins/Routing.kt
+++ src/main/kotlin/com/example/plugins/Routing.kt
@@ -1,11 +1,16 @@
 package com.example.plugins
 
 import io.ktor.server.application.*
+import io.ktor.server.auth.*
+import io.ktor.server.response.*
+import io.ktor.server.routing.*
 
 fun Application.configureRouting() {
     routing {
-        get("/") {
-            call.respondText("Hello World!")
+        authenticate {
+            get("/") {
+                call.respondText("Hello World!")
+            }
         }
     }
 }
--- src/main/kotlin/com/example/plugins/Security.kt
+++ src/main/kotlin/com/example/plugins/Security.kt
@@ -2,28 +2,27 @@ package com.example.plugins
 
 import com.auth0.jwt.JWT
 import com.auth0.jwt.algorithms.Algorithm
+import com.typesafe.config.ConfigFactory
 import io.ktor.server.application.*
 import io.ktor.server.auth.*
 import io.ktor.server.auth.jwt.*
+import io.ktor.server.config.*
 
 fun Application.configureSecurity() {
-    // Please read the jwt property from the config file if you are using EngineMain
-    val jwtAudience = "jwt-audience"
-    val jwtDomain = "https://jwt-provider-domain/"
-    val jwtRealm = "ktor sample app"
-    val jwtSecret = "secret"
+    val config = HoconApplicationConfig(ConfigFactory.load())
+    val jwtRealm = "sample"
+    val jwtSecret = config.property("security.jwt.secret").getString()
     authentication {
         jwt {
             realm = jwtRealm
             verifier(
-                    JWT
-                            .require(Algorithm.HMAC256(jwtSecret))
-                            .withAudience(jwtAudience)
-                            .withIssuer(jwtDomain)
-                            .build()
+                JWT
+                    .require(Algorithm.HMAC256(jwtSecret))
+                    .build()
             )
             validate { credential ->
-                if (credential.payload.audience.contains(jwtAudience)) JWTPrincipal(credential.payload) else null
+                JWTPrincipal(credential.payload)
+                // TODO 토큰 유효 여부만 확인 중 -> 클레임 값을 기준으로 실제 존재하는 유저인지에 대한 정보 검증 등의 절차 추가 가능
             }
         }
     }
